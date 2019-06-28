386c06c26dee50c9f1ac47f2dbd5138258e6fdb0
satish <satishmalkar94@gmail.com>
2019-06-28 23:41:01 +0530

    Feature ::changes in html code

--- javscript.js
+++ javscript.js
@@ -2,32 +2,43 @@ function Calculator () {
     this.evaluate = function(string){
         var res = string.split(" ");
 
-
-
         function multi(a,b){
-        mul=a*b;
-        console.log("mul",mul)
+        multplication=a*b;
+        console.log("multplication",multplication)
 
         }
 
-        function divi(a,b){
+        function div(a,b){
 
-            div=a/b;
-            console.log("div",div)
+            division=a/b;
+            console.log("division",division)
 
 
         }
         function add(a,b){
 
-            add=a+b;
-            console.log("add",add)
+            addition=a+b;
+            console.log("addition",addition)
 
 
         }
         function sub(a,b){
 
-            sub=a-b;
-            console.log("sub",sub)
+            substraction=a-b;
+            console.log("substraction",substraction)
+
+        }
+
+        function min(a,b){
+
+            a<b? minimum=a:minimum=b;
+            console.log("minimum",minimum)
+        }
+
+        function max(a,b){
+
+            a>b? maximum=a:maximum=b;
+            console.log("maximum",maximum)
 
 
         }
@@ -45,7 +56,7 @@ function Calculator () {
             l=res[0]
             m=res[2]
 
-            divi(l,m)
+            div(l,m)
         }
 
          else if(res[1] == '+')
@@ -62,13 +73,29 @@ function Calculator () {
 
             sub(l,m)
         }
-
+        else if(res[0] == 'minimum'){
+            l=res[1]
+            m=res[2]
+            min(l,m)
+        }
+        else if(res[0] == 'maximum'){
+            l=res[1]
+            m=res[2]
+            max(l,m)
+        }
     }
 
 
 }
     calculator = new Calculator()
     calculator.evaluate('20 * 2')
+    calculator.evaluate("20 - 2");
+    calculator.evaluate("20 / 2");
+    calculator.evaluate("20 + 2");
+    calculator.evaluate('maximum 20 2');
+    calculator.evaluate('minimum 20 2');
+    // calculator.evaluate("20 + 1 * 2");
+
 
 
 
